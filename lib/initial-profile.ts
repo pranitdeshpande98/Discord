009e7735673e7c8552a2dce02e40e8a027a753cb
Pranit Deshpande <71553492+pranitdeshpande98@users.noreply.github.com>
2024-09-28 17:09:53 -0400

    prisma model setup completed

---
+++ lib/initial-profile.ts
@@ -0,0 +1,12 @@
+import { currentUser, redirectToSignIn } from "@clerk/nextjs/server";
+import { db } from "@/lib/db";
+
+export const initialProfile = async () => {
+    const user = await currentUser();
+
+    if(!user){
+        return redirectToSignIn();
+    }
+
+   
+}
