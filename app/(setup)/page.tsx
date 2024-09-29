7418bdde622da0d930f618ee0a35f9f30fc5c784
Pranit Deshpande <71553492+pranitdeshpande98@users.noreply.github.com>
2024-09-29 16:09:01 -0400

    react hook form changes added for setting server name.

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,3 +1,4 @@
+import { InitialModal } from "@/components/modals/InitialModal";
 import { db } from "@/lib/db";
 import { initialProfile } from "@/lib/initial-profile";
 import { redirect } from "next/navigation";
@@ -18,6 +19,6 @@ const SetupPage = async () => {
     if(server){
         return redirect(`/servers/${server.id}`);
     }
-    return <div> Create a Server </div>
+    return <InitialModal />
 }
 export default SetupPage;
