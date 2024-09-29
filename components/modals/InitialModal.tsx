b97c923b28a9859878741fa5c519d7c63e1d591a
Pranit Deshpande <71553492+pranitdeshpande98@users.noreply.github.com>
2024-09-29 19:15:58 -0400

    server addition functionality successfully added.

--- components/modals/InitialModal.tsx
+++ components/modals/InitialModal.tsx
@@ -6,7 +6,7 @@ import {
     DialogFooter,
     DialogHeader,
     DialogTitle,  } from "@/components/ui/dialog"
-
+import axios from 'axios';
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod"; 
 import { useForm } from "react-hook-form";
@@ -22,6 +22,7 @@ import { Input } from "@/components/ui/input"
 import { Button } from "../ui/button";
 import { useEffect, useState } from "react";
 import { FileUpload } from "../file-upload";
+import { useRouter } from "next/navigation";
 
 const formSchema = z.object({
     name: z.string().min(1, {
@@ -36,6 +37,7 @@ const formSchema = z.object({
 export const InitialModal = () => {
 
     const [isMounted, setIsMounted] = useState(false);
+    const router = useRouter();
 
     useEffect(() => {
         setIsMounted(true);
@@ -50,7 +52,14 @@ export const InitialModal = () => {
 
     const isLoading = form.formState.isSubmitting;
     const onSubmit = async(values: z.infer<typeof formSchema>) => {
-        console.log(values);
+        try{
+            await axios.post("/api/servers", values);
+            form.reset();
+            router.refresh();
+            window.location.reload();
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     if(!isMounted){
