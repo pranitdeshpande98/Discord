7c06995194ac56062cfee3e171555908544522cf
Pranit Deshpande <71553492+pranitdeshpande98@users.noreply.github.com>
2024-10-02 15:44:48 -0400

    delete server for admin working as expected.

---
+++ components/modals/delete-server-modal.tsx
@@ -0,0 +1,67 @@
+"use client";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,  } from "@/components/ui/dialog"
+import { useModal } from "@/hooks/use-modal-store";
+import { Button } from "../ui/button";
+import { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+
+
+export const DeleteServerModal = () => {
+
+    const {isOpen, onClose, type, data } = useModal();
+    const isModalOpen = isOpen && type === 'deleteServer';
+    const {server} = data;
+    const router = useRouter();
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [isLoading, setIsLoading] = useState(false);
+
+    const onClick = async() => {
+        try{
+            setIsLoading(true);
+            await axios.delete(`/api/servers/${server?.id}`);
+            onClose();
+            router.refresh();
+            router.push("/");
+        } catch (error){    
+            console.log(error);
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    return (
+       <Dialog open={isModalOpen} onOpenChange={onClose}> 
+        <DialogContent className="bg-white text-black p-0 overflow-hidden">
+            <DialogHeader className="pt-8 px-6">
+            <DialogTitle className="text-2xl font-bold text-center">
+               Delete Server
+            </DialogTitle>
+
+            <DialogDescription className="text-center text-zinc-500">
+                Are you sure you want to do this? <br /> 
+                <span className="font-semibold text-indigo-500">{server?.name}</span> will be permanently deleted.
+            </DialogDescription>
+            </DialogHeader>
+            <DialogFooter className="bg-gray-100 px-6 py-4">
+                <div className="flex items-center justify-between w-full">
+                    <Button disabled={isLoading} onClick={onClose} variant="ghost">
+                        Cancel
+                    </Button>
+
+                    <Button disabled={isLoading} variant="primary" onClick={onClick}>
+                        Confirm
+                    </Button>
+                </div>
+            </DialogFooter>
+        </DialogContent>
+       </Dialog>
+    );
+}
