e1d5ea2bed395501444d1ad66024f905162fede5
Pranit Deshpande <71553492+pranitdeshpande98@users.noreply.github.com>
2024-10-06 16:29:25 -0400

    channel edit label issue fixed

--- components/modals/create-channel-modal.tsx
+++ components/modals/create-channel-modal.tsx
@@ -30,6 +30,7 @@ import {
   } from "@/components/ui/select"
 import { ChannelType } from "@prisma/client";
 import qs from "query-string";
+import { useEffect } from "react";
 
 const formSchema = z.object({
     name: z.string().min(1, {
@@ -44,19 +45,29 @@ const formSchema = z.object({
 
 export const CreateChannelModal = () => {
 
-    const { isOpen, onClose, type } = useModal();
+    const { isOpen, onClose, type, data } = useModal();
     const router = useRouter();
     const params = useParams();
 
     const isModalOpen = isOpen && type === 'createChannel';
+    const {channelType} = data;
+
     const form = useForm({
         resolver: zodResolver(formSchema),
         defaultValues:{
             name: "",
-            type: ChannelType.TEXT,
+            type: channelType ||  ChannelType.TEXT,
            
         }
     });
+
+    useEffect(()=> {
+        if(channelType){
+            form.setValue("type", channelType);
+        } else {
+            form.setValue("type", ChannelType.TEXT);
+        }
+    }, [channelType, form]);
 
     const isLoading = form.formState.isSubmitting;
     const onSubmit = async(values: z.infer<typeof formSchema>) => {
